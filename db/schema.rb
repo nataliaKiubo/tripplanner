672d948ad4b7d164eb3c83ab7ad636b33e54d25e
welton torres <weltonmail@gmail.com>
2022-08-30 12:27:18 +0200

    I created the Basic Structure of Geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_142934) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_091038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_142934) do
     t.bigint "trip_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["trip_id"], name: "index_stops_on_trip_id"
   end
 
