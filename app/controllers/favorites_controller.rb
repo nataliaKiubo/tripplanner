78d6d5646a38a925a885e51716dcbf53ed1b4729
Natalia Ramirez <natalia.ramirez@hotmail.com>
2022-08-31 22:18:14 +0200

    display trips in favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,22 @@
 class FavoritesController < ApplicationController
   def index
     @favorites = policy_scope(Favorite)
-    sql_query = <<~SQL
-      favorites.user_id = :query
-      OR trips.user_id = :query
-    SQL
-    @favorites = Favorite.joins(:trip).where(sql_query, query: current_user.id)
+
+    #@favorite_trips = Favorite.joins(:trips, :users)
+    #@favorites = Favorite.all
+    @user = User.find(params[:user_id])
+    @favorites = Favorite.where(favoritor_id: @user.id)
+    @favorite_trips = @favorites.map do |favorite|
+        Trip.find(favorite.favoritable_id)
+    end
+
+
+    # sql_query = <<~SQL
+    #   favorites.user_id = :query
+    #   AND trips.user_id = :query
+    # SQL
+    # @favorites = Favorite.joins(:trip).where(sql_query, query: current_user.id)
+
     authorize @favorites
   end
 end
