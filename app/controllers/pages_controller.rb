3efee3d010d717ffff773011d9e08b99a6d0b178
Natalia Ramirez <natalia.ramirez@hotmail.com>
2022-09-09 11:02:55 +0200

    trips for front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @trips = Trip.last(6)
-    @users = User.last(6)
+    @trips = Trip.where(id: [35, 37, 60, 38, 61, 97])
+    @users = User.where(id: [13, 14, 15, 16, 17, 18])
   end
 end
